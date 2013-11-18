0da2b3917c4d855f7a9dfd909d703383be549fc3
Zoltan Arvai <zarvai@inf.u-szeged.hu>
2013-11-18 10:11:07 +0100

    - updating Qt 5.2.0 to current revision

--- qt5-env.bat
+++ qt5-env.bat
@@ -1,5 +1,5 @@
-set QT_WEEKLY_REV=Qt5.2.0-qtwe-r1
-set WEEKLY_QT5_HASH="cf08b0d1091451f1762bbef7981cb1d9475f53b2"
+set QT_WEEKLY_REV=Qt5.2.0-qtwe-r2
+set WEEKLY_QT5_HASH="7c73e564bd61faa38fdd51947f8d98c03230f104"
 
 set QT5_MODULES=qtxmlpatterns qtdeclarative qtquickcontrols qtscript qtquick1 qttools qtx11extras
 
